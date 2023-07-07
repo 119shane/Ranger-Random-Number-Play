0c5f24d3c56571c71513c2f403b6d8ae801cd8e7
Shane Amal <21stwebwizard@gmail.com>
2023-07-07 12:48:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const firstNumber = document.getElementById("1st_number")
+const secondNumber = document.getElementById("2nd_number")
+const doItBtn = document.getElementById("do_it_btn")
+
+const table = document.getElementById("table")
+const error = document.getElementById("error")
+
+const allNumberBtn = document.querySelectorAll(".numbers")
+
+
+doItBtn.addEventListener('click', (e)=> {
+    e.preventDefault
+    numberGenerator(firstNumber.value, secondNumber.value)
+
+})
+
+function numberGenerator (a, b) {
+    const difference = b-a
+
+    randomlyGeneratedNumArr = []
+
+    if (difference === 50 || difference > 50) {
+        
+        for(let i = 0; i < 50; i++){
+            let randomNumber = Number(a) + Number(Math.floor(Math.random() * difference))
+            const button = document.createElement("button")
+            button.classList.add("numbers")
+            button.setAttribute("id", i)
+            button.setAttribute("value", randomNumber)
+            button.setAttribute("name", randomNumber)
+            randomlyGeneratedNumArr.push(button)
+            randomlyGeneratedNumArr.map(item => table.appendChild(item))
+            error.textContent = ''
+        }
+    } 
+    else {
+        error.textContent = "please enter numbers in correct range"
+    }
+}
+
+
+
