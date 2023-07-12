0834a8370a7fd4d142a75ab9ec9b08b7db26c9b1
Shane Amal <21stwebwizard@gmail.com>
2023-07-12 19:11:11 +0530

    add new code

--- script.js
+++ script.js
@@ -1,42 +1,92 @@
-const firstNumber = document.getElementById("1st_number")
-const secondNumber = document.getElementById("2nd_number")
-const doItBtn = document.getElementById("do_it_btn")
-
-const table = document.getElementById("table")
-const error = document.getElementById("error")
-
-
-doItBtn.addEventListener('click', (e)=> {
-    e.preventDefault
-    numberGenerator(firstNumber.value, secondNumber.value)
-
-})
-
-function numberGenerator (a, b) {
-    const difference = b-a
-
-    randomlyGeneratedNumArr = []
-
-    if (difference === 50 || difference > 50) {
-        
-        for(let i = 0; i < 50; i++){
-            let randomNumber = Number(a) + Number(Math.floor(Math.random() * difference))
-            const button = document.createElement("button")
-            button.classList.add("numbers")
-            button.setAttribute("id", i)
-            button.setAttribute("value", randomNumber)
-            button.setAttribute("name", randomNumber)
-            randomlyGeneratedNumArr.push(button)
-            randomlyGeneratedNumArr.map(item => table.appendChild(item))
-            error.textContent = ''
-            doItBtn.disabled = true
-            doItBtn.style.cursor = "notAllowed"
+    // Function to generate random numbers without duplicates
+    function generateNumbers() {
+        var rangeStart = parseInt(document.getElementById('rangeStart').value);
+        var rangeEnd = parseInt(document.getElementById('rangeEnd').value);
+  
+        var numbers = [];
+        while (numbers.length < 50) {
+          var number = Math.floor(Math.random() * (rangeEnd - rangeStart + 1)) + rangeStart;
+          if (!numbers.includes(number)) {
+            numbers.push(number);
+          }
         }
-    } 
-    else {
-        error.textContent = "please enter numbers in correct range"
-    }
-}
-
-
-
+  
+        displayNumbers(numbers);
+      }
+  
+      // Function to display generated numbers
+      function displayNumbers(numbers) {
+        var numberContainer = document.getElementById('numberContainer');
+        numberContainer.innerHTML = '';
+  
+        for (var i = 0; i < numbers.length; i++) {
+          var numberElement = document.createElement('span');
+          numberElement.setAttribute('class', 'number');
+          numberElement.setAttribute('onclick', 'selectNumber(' + numbers[i] + ')');
+          numberElement.textContent = numbers[i];
+          numberContainer.appendChild(numberElement);
+        }
+      }
+  
+      // Function to select a number
+      function selectNumber(number) {
+        var selectedNumbers = JSON.parse(localStorage.getItem('selectedNumbers')) || [];
+        var index = selectedNumbers.indexOf(number);
+  
+        if (index === -1 && selectedNumbers.length < 10) {
+          selectedNumbers.push(number);
+        } else if (index !== -1) {
+          selectedNumbers.splice(index, 1);
+        }
+  
+        localStorage.setItem('selectedNumbers', JSON.stringify(selectedNumbers));
+        displaySelectedNumbers(selectedNumbers);
+      }
+  
+      // Function to display selected numbers
+      function displaySelectedNumbers(selectedNumbers) {
+        var numberContainer = document.getElementById('numberContainer');
+        var numberElements = numberContainer.getElementsByClassName('number');
+  
+        for (var i = 0; i < numberElements.length; i++) {
+          var number = parseInt(numberElements[i].textContent);
+          if (selectedNumbers.includes(number)) {
+            numberElements[i].classList.add('selected');
+          } else {
+            numberElements[i].classList.remove('selected');
+          }
+        }
+  
+        var selectedNumbersContainer = document.getElementById('selectedNumbers');
+        selectedNumbersContainer.innerHTML = selectedNumbers.join(', ');
+  
+        var addResult = document.getElementById('addResult');
+        addResult.textContent = 'Addition Result: ' + selectedNumbers.reduce((a, b) => a + b, 0);
+  
+        var multiplyResult = document.getElementById('multiplyResult');
+        multiplyResult.textContent = 'Multiplication Result: ' + selectedNumbers.reduce((a, b) => a * b, 1);
+      }
+  
+      // Function to display generated URLs
+      function displayGeneratedUrls() {
+        var generatedUrls = JSON.parse(localStorage.getItem('generatedUrls')) || [];
+        var generatedUrlsContainer = document.getElementById('generatedUrls');
+        generatedUrlsContainer.innerHTML = '';
+  
+        generatedUrls.forEach(function(url) {
+          var listItem = document.createElement('li');
+          var link = document.createElement('a');
+          link.href = url;
+          link.textContent = url;
+          listItem.appendChild(link);
+          generatedUrlsContainer.appendChild(listItem);
+        });
+      }
+  
+      // Load selected numbers and generated URLs on page load
+      window.onload = function() {
+        var selectedNumbers = JSON.parse(localStorage.getItem('selectedNumbers')) || [];
+        displaySelectedNumbers(selectedNumbers);
+  
+        displayGeneratedUrls();
+      };
